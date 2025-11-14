d5fa6d8945b847ba3870d3324bf2af3c4c29f0b8
mohameddehbi <dehbi205@gmail.com>
2025-11-14 10:32:57 +0100

    code initiale des page afficher_matches et afficher_joueurs

---
+++ PHP/Vue/afficher_joueurs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form methode ="POST" action="">
+        <table border="1px solid white" border-collapse= "collapse" width="850px">
+               <tr><th><label for="nom">Nom</label></th>
+               <th><label for="prenom">Prénom</label></th>
+               <th><label for="NumLic">Numero de licence</label></th>
+               <th><label for="DateNaiss">Date de naissance</label></th>
+               <th><label for="Taille">Taille</label></th>
+               <th><label for="Poids">Poids</label></th>
+               <th><label for="Statut">Statut</label></th>
+               <th><label for="PostePref">Poste préféré</label></th>
+               </tr>
+        </table>
+
+        <input type="submit" value="Ajouter un joueur">
+        <input type="submit" value="Moodifer un joueur">
+        <input type="submit" value="Supprimer un joueur">
+        <input type="submit" value="Voir les détails du joueur">
+    </form>
+</body>
+</html>
---
+++ PHP/Vue/afficher_matches.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form methode ="POST" action="">
+        <table border="1px solid white" border-collapse= "collapse" width="850px">
+               <tr><th><label for="Date">Date</label></th>
+               <th><label for="Heure">Heure</label></th>
+               <th><label for="Adversaire">Adversaire</label></th>
+               <th><label for="Lieu">Lieu</label></th>
+               <th><label for="Résultat">Résultat</label></th>
+               <th><label for="Statut">Statut</label></th>
+               </tr>
+        </table>
+
+        <input type="submit" value="Voir feuille du match">
+        <input type="submit" value="Préparer la feuille de match">
+        <input type="submit" value="Modifier le match">
+        <input type="submit" value="Supprimer le match">
+        <input type="submit" value="Ajouter un match">
+
+    </form>
+</body>
+</html>
