e4bc02318a34b279427ad6dafb396af0c5af1b81
Kenny HILAIRE <kenny.hilaire@etu.iut-tlse3.fr>
2025-12-12 10:13:53 +0100

    MenuPrincipale modification du css

--- PHP/Vue/menuPrincipale.php
+++ PHP/Vue/menuPrincipale.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion d'une équipe de sport</title>
-    <link rel="stylesheet" href="mp.css">
+    <link rel="stylesheet" href="menuPrincipale.css">
 </head>
 <body>
     <h1> Content de vous revoir coach, à vous de jouer maintenant!!</h1>
     <section class="menu">
-        <div class= feuilleMatch>
+        <div class= "feuilleMatch">
             <img src="PHOTO/feuilleMatch.jpg" alt="Liste/feuilleMatch" class="mp">
             <h3>Acceder à la LISTES des matchs svp</h3>   
             <form action="afficher_matches.php" method="GET">
@@ -33,5 +33,7 @@
 </section>
 </body>
 </html>
+<?php
 
+?>
 
